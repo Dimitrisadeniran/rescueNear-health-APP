16563093c92b1a3486b37e791a4ee54b68d8c015
Dimitrisadeniran <147705095+Dimitrisadeniran@users.noreply.github.com>
2025-09-06 17:37:09 -0700

    Update routes.js

--- Rescuenear backend/routes.js
+++ Rescuenear backend/routes.js
@@ -1,10 +1,11 @@
 // routes.js
 const express = require('express');
 const router = express.Router();
-const { signup, login, verify } = require('./controllersauthcontrollers'); // ✅ corrected path
+const { signup, login, verify } = require('./controllersauthcontrollers');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/verify', verify);
 
 module.exports = router;
+
